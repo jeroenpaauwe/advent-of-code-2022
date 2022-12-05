052b286829c019218d0db0a1304257b708e1ffe6
jeroen.paauwe <jeroen.paauwe@kempen.nl>
2022-12-05 20:20:08 +0100

    small edit

--- day_4/4.py
+++ day_4/4.py
@@ -15,13 +15,10 @@ for line in data:
     
 print(count) # part 1 solution
 
-
 with open("4.txt", "r") as f:
     data = re.findall(r"(\d+)-(\d+),(\d+)-(\d+)", f.read())
     data = [[int(e) for e in row] for row in data]
 
-contained = [(a <= c <= d <= b or c <= a <= b <= d) for a, b, c, d in data]
 overlap = [max(a, c) <= min(b, d) for a, b, c, d in data]
 
-print(sum(contained))
-print(sum(overlap))
+print(sum(overlap))  # part 2 solution
