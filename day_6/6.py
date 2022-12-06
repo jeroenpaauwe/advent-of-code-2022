dc8acc71aff3e8975be300dde4ac79c794fe1020
jeroen.paauwe <jeroen.paauwe@kempen.nl>
2022-12-06 13:27:27 +0100

    added list comprehension solution

--- day_6/6.py
+++ day_6/6.py
@@ -14,3 +14,16 @@ for i in range(0,len(signal)-13):
         break
         
 print(i+14) # solution part 2
+
+packets = [set(signal[i:i+4]) for i in range(0,len(signal)-3)]
+
+fours = [len(x) for x in packets]
+
+print(fours.index(4)+4) # solution part 1 (using list comprehension)
+
+packets = [set(signal[i:i+14]) for i in range(0,len(signal)-13)]
+
+fourteens = [len(x) for x in packets]
+
+print(fourteens.index(14)+14) # solution part 2 (using list comprehension)
+
