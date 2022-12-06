4f1dc3d567e93c5158300bd5b7a578b9e2d45235
jeroen.paauwe <jeroen.paauwe@kempen.nl>
2022-12-06 10:38:17 +0100

    code improvement

--- day_6/6.py
+++ day_6/6.py
@@ -1,21 +1,16 @@
-with open("6.txt") as f:
-   signal = f.read()
+with open("day_6/6.txt") as f:
+    signal = f.read()
 
-for i in range(0,len(signal)-3):   
-    if (signal[i:i+4].count(signal[i]) == 1 and signal[i:i+4].count(signal[i+1]) == 1 and 
-        signal[i:i+4].count(signal[i+2]) == 1 and signal[i:i+4].count(signal[i+3]) == 1):
+for i in range(0,len(signal)-3):
+    packet = signal[i:i+4]
+    if len(set(packet)) == 4:
         break
         
 print(i+4) # solution part 1
     
 for i in range(0,len(signal)-13):   
-    if (signal[i:i+14].count(signal[i]) == 1 and signal[i:i+14].count(signal[i+1]) == 1 and 
-        signal[i:i+14].count(signal[i+2]) == 1 and signal[i:i+14].count(signal[i+3]) == 1 and
-        signal[i:i+14].count(signal[i+4]) == 1 and signal[i:i+14].count(signal[i+5]) == 1 and
-        signal[i:i+14].count(signal[i+6]) == 1 and signal[i:i+14].count(signal[i+7]) == 1 and
-        signal[i:i+14].count(signal[i+8]) == 1 and signal[i:i+14].count(signal[i+9]) == 1 and
-        signal[i:i+14].count(signal[i+10]) == 1 and signal[i:i+14].count(signal[i+11]) == 1 and
-        signal[i:i+14].count(signal[i+12]) == 1 and signal[i:i+14].count(signal[i+13]) == 1):
+    packet = signal[i:i+14]
+    if len(set(packet)) == 14:
         break
         
 print(i+14) # solution part 2
