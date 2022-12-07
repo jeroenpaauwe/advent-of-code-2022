1d6aaede69e4c0f196f221b7e85f31f003fde37a
jeroen.paauwe <jeroen.paauwe@kempen.nl>
2022-12-07 21:33:30 +0100

    day 7

--- day_7/7.py
+++ day_7/7.py
@@ -1,3 +1,24 @@
-with open("7.txt") as f:
-   signal = f.read()
+import re
 
+with open("7.txt") as f:
+    data = iter(f.read().splitlines())
+
+sizes = []
+stack = [0]
+while stack:
+    line = next(data, "$ cd ..")
+    if line == "$ cd ..":
+        child = stack.pop()
+        if stack:
+            sizes.append(child)
+            stack[-1] += child
+    elif cd := re.match(r"\$ cd .+", line):
+        stack.append(0)
+    elif file := re.match(r"(\d+) .+", line):
+        stack[-1] += int(file.group(1))
+
+space_needed = 30_000_000 - (70_000_000 - sizes[-1])
+sizes = sorted(sizes)
+
+print(sum(elt for elt in sizes if elt <= 100_000))
+print(next(elt for elt in sizes if elt >= space_needed))
